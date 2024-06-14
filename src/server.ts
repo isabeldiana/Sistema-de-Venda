388972d6831e0a7acb51c1ee206536263b088cd2
isabeldiana <isabel_diana@outlook.com>
2024-06-13 21:25:53 -0300

    install: Cors installation and added.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 require('dotenv').config();
 import express from 'express';
+import cors from 'cors';
 import customerRouter from './routers/routers';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api',customerRouter);
 
