0a13e587b05e2f6c30fabccc1355659dc8fcf337
isabeldiana <isabel_diana@outlook.com>
2024-07-30 20:16:51 -0300

    update: New routers

--- src/routers/routers.ts
+++ src/routers/routers.ts
@@ -2,7 +2,12 @@ import { Router } from 'express';
 import costumer from '../controllers/customers/customer';
 import products from '../controllers/products/products';
 import sales from '../controllers/sales/sales';
+import users from '../controllers/users/users';
 const router = Router();
+
+//user
+
+router.post('/createUser', users.createUser );
 
 //customers
 router.post('/createCustomer', costumer.createCustomer);
@@ -14,10 +19,17 @@ router.put('/updateCustomer/:id', costumer.updateCustomer)
  router.post('/createProduct', products.createdProduct);
  router.put('/updateProduct/:id', products.updateProduct);
  router.put('/updateStock/:id', products.stockEntry);
+ router.get('/stock', products.showStok);
+
 
  // sales
 
  router.post('/createSales', sales.createSales);
  router.get('/showSale/:id', sales.showSaleId)
  router.get('/showSale', sales.showSale)
+ router.get('/productBestSelling', sales.productBestSelling)
+
+ //user 
+
+
 export default router;
