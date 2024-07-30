cac6507ccca0226a6771404b8c4130b4603025e0
isabeldiana <isabel_diana@outlook.com>
2024-07-30 20:40:44 -0300

    fix: Fixed name router

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
-import customerRouter from './routers/routers';
+import Router from './routers/routers';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api',customerRouter);
+app.use('/api', Router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT;
+app.listen(PORT);
